af9f7498915b7262ca14d7cfebc5620ec3821c7d
Marc Dingena <marc.dingena@gmail.com>
2022-01-04 16:57:07 +0800

    Added automatic linting

--- encode.ts
+++ encode.ts
@@ -19,7 +19,7 @@ const pommel = createPrefab(Prefab.Pommel_Large_Square)
 const handle = createPrefab(Prefab.Handle_Short)
   .setMaterial('Redwood')
   .useSlot('Slot_Large_SwordType_Craft_1', guard)
-  .useSlot('Slot_PommelType_2', pommel)
+  .useSlot('Slot_PommelType_2', pommel);
 
 /* Print the handle's string (which includes all of the slotted prefabs). */
 handle.print();
@@ -27,8 +27,8 @@ handle.print();
 /**
  * To get the string, Node needs to run this file.
  * The string will be printed in the same terminal that calls this script.
- * 
+ *
  * In your VS Code terminal, type: npm run encode
- * 
+ *
  * The string will appear for you to copy paste into the ATT console.
  */
