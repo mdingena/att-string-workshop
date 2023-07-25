3c4434e1cd08f45ec7238b139e71bd37dff476d7
Marc Dingena <marc.dingena@gmail.com>
2023-07-25 14:27:32 +0800

    Refactor code to support att-string-transcoder v3

---
+++ creations/example-longsword.ts
@@ -0,0 +1,62 @@
+import { Prefab } from 'att-string-transcoder';
+
+/* Create a mythril longsword blade. */
+const blade = new Prefab('Large_Longsword_Blade').setMaterial('Mythril');
+
+/* Create a palladium guard and attach the blade. */
+const guard = new Prefab('Guard')
+  .setMaterial('CarsiAlloy')
+  .addChildPrefab('Slot_SwordType_39370', blade);
+
+/* Create a valyan pommel. */
+const pommel = new Prefab('Pommel_Large_Square').setMaterial('EvinonSteelAlloy');
+
+/* Create a handle and attach the guard and pommel. */
+const handle = new Prefab('Handle_Short')
+  .setMaterial('Redwood')
+  .addChildPrefab('Slot_Multi_6138', guard)
+  .addChildPrefab('Slot_Large_SwordType_Craft_54356', pommel);
+
+/**
+ * To get this save string, type: npm run encode creations/example-longsword.ts
+ * The string will appear for you to copy paste into the ATT console.
+ */
+export default handle;
+
+/**
+ * It's possible export multiple creations from a file. This lets you easily make variants
+ * of a creation.
+ */
+const handleWithDamagedBlade = handle.clone();
+
+handleWithDamagedBlade
+  .findChildPrefab('Guard')
+  ?.setIntegrity(0.2)
+  .findChildPrefab('Large_Longsword_Blade')
+  ?.setIntegrity(0.2);
+
+/**
+ * To get this save string, type: npm run encode creations/example-longsword.ts damagedSword
+ */
+export const damagedSword = handleWithDamagedBlade; // Named export syntax.
+
+const shortSword = handle.clone();
+
+shortSword
+  .clone()
+  .findChildPrefab('Guard')
+  ?.removeChildPrefab('Large_Longsword_Blade')
+  .addChildPrefab('Slot_SwordType_39370', new Prefab('Short_Sword_Blade').setMaterial('Mythril'));
+
+/**
+ * To get this save string, type: npm run encode creations/example-longsword.ts shortSword
+ */
+export { shortSword }; // Alternative named export syntax.
+
+/**
+ * To get this save string, type: npm run encode creations/example-longsword.ts justTheBlade
+ */
+export const justTheBlade = handle
+  .clone()
+  .findChildPrefab('Guard')
+  ?.findChildPrefab('Large_Longsword_Blade');
---
+++ scripts/README.md
@@ -0,0 +1,3 @@
+# Scripts
+
+The scripts in this directory are designed for internal use by the Workshop. Please do not modify any of these scripts.
