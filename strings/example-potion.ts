47024dff6e897c4d6a64e9f943aa8555f4e91e41
Marc Dingena <marc.dingena@gmail.com>
2023-08-25 21:48:34 +0800

    Create example-potion.ts

---
+++ strings/example-potion.ts
@@ -0,0 +1,7 @@
+/**
+ * The save string in this file corresponds to the creations/example-potion.ts file.
+ *
+ * To decode this save string, type: npm run decode strings/example-potion.ts
+ * The string will appear for you to copy paste into the ATT console.
+ */
+export default `23644,136,23644,0,0,0,0,0,0,1065353216,1065353216,4179293747,650,536870922,2952790016,34684928,34885632,34963456,0,872,402653184,268435481,2692743168,189,1212227584,39,656392192,6,2601527296,0,12582925,1191182352,3539992625,1283457024,0,0,|1,4179293747,1,`;
